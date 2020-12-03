9ee51bac9c644830b842ba6dcd8fa47f33b00c0a
Solipugids <ch.w@mail.scut.edu.cn>
2020-12-03 22:20:06 +0800

    Fix bugs

--- content/home/bak_exp.m
+++ content/home/bak_exp.m
@@ -18,27 +18,23 @@ date_format = "Jan 2006"
 #   Leave `date_end` empty if it's your current employer.
 #   Begin/end multi-line descriptions with 3 quotes `"""`.
 [[experience]]
-  title = "CEO"
-  company = "GenCoin"
-  company_url = ""
-  location = "California"
-  date_start = "2017-01-01"
+  title = "Postdoctor"
+  company = "School of Chinese Medicine, Hong Kong Baptist University"
+  company_url = "scm.hkbu.edu.hk"
+  location = "Hong Kong SAR"
+  date_start = "2015-10-01"
   date_end = ""
   description = """
-  Responsibilities include:
-  
-  * Analysing
-  * Modelling
-  * Deploying
+  test
   """
 
 [[experience]]
-  title = "Professor"
-  company = "University X"
-  company_url = ""
-  location = "California"
-  date_start = "2016-01-01"
-  date_end = "2016-12-31"
-  description = """Taught electronic engineering and researched semiconductor physics."""
+  title = "Senior Research Assistant"
+  company = "Department of Computer Science, Hong Kong Baptist University"
+  company_url = "comp.hkbu.edu.hk"
+  location = "Hong Kong SAR"
+  date_start = "2014-01-14"
+  date_end = "2015-09-31"
+  description = """test"""
 
 +++
